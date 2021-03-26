9eac2d23f81425a49a519bb04ef5a3d683aa5f54
joseph <owningmoon@gmail.com>
2021-03-26 15:54:15 +0700

    update

--- app/src/main/java/com/pemerintah/beaconsampah/splash.kt
+++ app/src/main/java/com/pemerintah/beaconsampah/splash.kt
@@ -6,6 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.livinglifetechway.quickpermissions_kotlin.runWithPermissions
 import kotlinx.android.synthetic.main.activity_splash.*
+import com.github.javiersantos.appupdater.AppUpdater
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.net.Uri
+import com.github.javiersantos.appupdater.enums.UpdateFrom
+
 
 class splash : AppCompatActivity() {
 
@@ -16,11 +23,28 @@ class splash : AppCompatActivity() {
         getSupportActionBar()!!.hide()
         act = this
         methodWithPermissions()
+        update()
+//        val appUpdater = AppUpdater(this)
+//            .setTitleOnUpdateAvailable("Update available")
+//            .setContentOnUpdateAvailable("Check out the latest version available of my app!")
+//            .setTitleOnUpdateNotAvailable("Update not available")
+//            .setContentOnUpdateNotAvailable("No update available. Check for updates again later!")
+//            .setButtonUpdate("Update now?")
+//            .setButtonUpdateClickListener(update())
+//
+//        .setButtonDoNotShowAgain("Huh, not interested")
+//
+//            .setCancelable(false) // Dialog could not be dismissab
+//            .start()
         buttonNexttoDashboard.setOnClickListener({
             var intent = Intent(act, dashboard::class.java)
             finish()
             startActivity(intent)
         })
+    }
+    fun update()
+    {
+        var test =  Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/josephkristanto10/kantor/tree/master"))
     }
     fun methodWithPermissions() = runWithPermissions(Manifest.permission.CAMERA,  Manifest.permission.READ_EXTERNAL_STORAGE,  Manifest.permission.WRITE_EXTERNAL_STORAGE,  Manifest.permission.ACCESS_FINE_LOCATION,  Manifest.permission.ACCESS_COARSE_LOCATION) {
 
